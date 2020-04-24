7d9a14a69abffcc0ddedff253717eefdeb721207
Anderson Begossi <andersonbegossi@gmail.com>
2020-04-24 16:04:14 -0300

    Configured Reactotron

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import 'react-native-gesture-handler';
-
 import React from 'react';
 import { StatusBar } from 'react-native';
+
+import './config/ReactotronConfig';
 
 import { NavigationContainer } from '@react-navigation/native';
 
