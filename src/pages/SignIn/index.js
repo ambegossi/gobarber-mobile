d9bb94c4d506aff1895d8c3c02c4bc831df3279c
Anderson Begossi <andersonbegossi@gmail.com>
2020-04-24 14:40:38 -0300

    Added Input and Button

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Text } from 'react-native';
 
 import Background from '~/components/Background';
+import Input from '~/components/Input';
+import Button from '~/components/Button';
 
 // import { Container } from './styles';
 
@@ -9,6 +11,14 @@ export default function SignIn() {
   return (
     <Background>
       <Text>SignIn</Text>
+
+      <Input
+        style={{ marginTop: 30 }}
+        icon="call"
+        placeholder="Digite seu nome"
+      />
+
+      <Button>Entrar</Button>
     </Background>
   );
 }
