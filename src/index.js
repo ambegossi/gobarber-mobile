6e0c0083b10baddbf4abb8f3f4fe55c14801ed95
Anderson Begossi <andersonbegossi@gmail.com>
2020-04-24 15:18:46 -0300

    Configured StatusBar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import 'react-native-gesture-handler';
 
 import React from 'react';
+import { StatusBar } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 
@@ -9,6 +10,7 @@ import App from './App';
 export default function Index() {
   return (
     <NavigationContainer>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <App />
     </NavigationContainer>
   );
