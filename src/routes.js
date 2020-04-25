7b90cf8c59e70491d311db5be92b01987c5de56f
Anderson Begossi <andersonbegossi@gmail.com>
2020-04-25 11:32:30 -0300

    Added initial route

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
+import Dashboard from './pages/Dashboard';
+
 const Stack = createStackNavigator();
+const Tabs = createBottomTabNavigator();
 
 export default function createRouter(isSigned = false) {
   return !isSigned ? (
@@ -13,5 +19,27 @@ export default function createRouter(isSigned = false) {
       <Stack.Screen name="SignIn" component={SignIn} />
       <Stack.Screen name="SignUp" component={SignUp} />
     </Stack.Navigator>
-  ) : null;
+  ) : (
+    <Tabs.Navigator
+      tabBarOptions={{
+        activeTintColor: '#FFF',
+        inactiveTintColor: 'rgba(255, 255, 255, 0.6)',
+        style: {
+          backgroundColor: '#8d41a8',
+        },
+        keyboardHidesTabBar: true,
+      }}
+    >
+      <Tabs.Screen
+        name="Dashboard"
+        component={Dashboard}
+        options={{
+          tabBarLabel: 'Agendamentos',
+          tabBarIcon: ({ color }) => (
+            <Icon name="event" size={20} color={color} />
+          ),
+        }}
+      />
+    </Tabs.Navigator>
+  );
 }
