79867b0e32d8f8a35f5b437baa3a71b4feefe450
Anderson Begossi <andersonbegossi@gmail.com>
2020-04-27 20:22:29 -0300

    Added list of providers

--- src/pages/New/SelectProvider/index.js
+++ src/pages/New/SelectProvider/index.js
@@ -1,8 +1,51 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect, useState } from 'react';
 
-// import { Container } from './styles';
+import api from '~/services/api';
 
-export default function SelectProvider() {
-  return <View />;
+import Background from '~/components/Background';
+
+import { Container, ProvidersList, Provider, Avatar, Name } from './styles';
+
+export default function SelectProvider({ navigation }) {
+  const [providers, setProviders] = useState([]);
+
+  useEffect(() => {
+    async function loadProviders() {
+      const response = await api.get('providers');
+
+      setProviders(response.data);
+    }
+
+    loadProviders();
+  }, []);
+
+  return (
+    <Background>
+      <Container>
+        <ProvidersList
+          data={providers}
+          keyExtractor={(provider) => String(provider.id)}
+          renderItem={({ item: provider }) => (
+            <Provider
+              onPress={() =>
+                navigation.navigate('SelectDateTime', { provider })
+              }
+            >
+              <Avatar
+                source={{
+                  uri: provider.avatar
+                    ? provider.avatar.url.replace(
+                        'localhost:3333',
+                        '3017d64b.ngrok.io'
+                      )
+                    : `https://api.adorable.io/avatar/50/${provider.name}.png`,
+                }}
+              />
+              <Name>{provider.name}</Name>
+            </Provider>
+          )}
+        />
+      </Container>
+    </Background>
+  );
 }
