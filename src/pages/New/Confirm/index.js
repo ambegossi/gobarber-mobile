381ad4153a25ef5e3c07f514543d7ab11c2c1101
Anderson Begossi <andersonbegossi@gmail.com>
2020-04-27 22:43:10 -0300

    Added appointment confirmation

--- src/pages/New/Confirm/index.js
+++ src/pages/New/Confirm/index.js
@@ -1,8 +1,51 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useMemo } from 'react';
+import { formatRelative, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
-// import { Container } from './styles';
+import Background from '~/components/Background';
 
-export default function Confirm() {
-  return <View />;
+import api from '~/services/api';
+
+import { Container, Avatar, Name, Time, SubmitButton } from './styles';
+
+export default function Confirm({ navigation, route }) {
+  const { provider, time } = route.params;
+
+  const dateFormatted = useMemo(
+    () => formatRelative(parseISO(time), new Date(), { locale: pt }),
+    [time]
+  );
+
+  async function handleAddAppointment() {
+    await api.post('appointments', {
+      provider_id: provider.id,
+      date: time,
+    });
+
+    navigation.navigate('Dashboard');
+  }
+
+  return (
+    <Background>
+      <Container>
+        <Avatar
+          source={{
+            uri: provider.avatar
+              ? provider.avatar.url.replace(
+                  'localhost:3333',
+                  '3017d64b.ngrok.io'
+                )
+              : `https://api.adorable.io/avatar/50/${provider.name}.png`,
+          }}
+        />
+
+        <Name>{provider.name}</Name>
+        <Time>{dateFormatted}</Time>
+
+        <SubmitButton onPress={handleAddAppointment}>
+          Confirmar agendamento
+        </SubmitButton>
+      </Container>
+    </Background>
+  );
 }
